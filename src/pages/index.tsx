33edeff01b7541b0717e281501bdb5efe0537905
Tanner <tanner.fleming@gmail.com>
2023-11-03 21:07:57 -0500

    yests te server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.post.getAll.useQuery();
 
   return (
     <>
@@ -15,7 +16,13 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-         
+         <div >
+          {data?.map((post) => (
+          <div key={post.id}>
+            {post.content}
+          </div>
+          ))}
+         </div>
         </div>
       </main>
     </>
