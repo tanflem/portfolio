f484d93b91d7712bda920ccd41722b9213331761
Tanner <tanner.fleming@gmail.com>
2024-03-08 22:17:50 -0600

    Add framer-motion library and new ProjectTag component

--- app/components/ProjectSection.tsx
+++ app/components/ProjectSection.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+"use client";
+import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+interface Project {
+    id: number;
+    title: string;
+    description: string;
+    imgUrl: string;
+    gitUrl: URL;
+    previewUrl: URL;
+    tag: string[];
+}
 
-const PROJECTS = [
+const projectsData = [
     {
         id: 1,
         title: "Project 1",
         description: "This is a project that I worked on",
         imgUrl: "/images/profile.jpg",
-        gitUrl: "https://github.com",
-        previewUrl: "https://github.com",
+        gitUrl: new URL("https://github.com"),
+        previewUrl: new URL("https://github.com"),
         tag: ["All", "Web"]
     },
     {
@@ -16,8 +28,8 @@ const PROJECTS = [
         title: "Project 2",
         description: "This is a project that I worked on",
         imgUrl: "/images/projects/2.webp",
-        gitUrl: "https://github.com",
-        previewUrl: "https://github.com",
+        gitUrl: new URL("https://github.com"),
+        previewUrl: new URL("https://github.com"),
         tag: ["All", "Web"]
     },
     {
@@ -25,30 +37,74 @@ const PROJECTS = [
         title: "Senior Design USAF Dashboard",
         description: "This is a project that I worked on",
         imgUrl: "/images/projects/3.png",
-        gitUrl: "https://github.com",
-        previewUrl: "https://github.com",
+        gitUrl: new URL("https://github.com"),
+        previewUrl: new URL("https://github.com"),
         tag: ["All", "Web"]
     }
 ]
 
-const ProjectSection = () => {
-    return (
-        <>
-            <h2>My Projects</h2>
-            <div>
-                {PROJECTS.map((project) => (
-                    <ProjectCard
-                        key={project.id}
-                        title={project.title}
-                        description={project.description}
-                        imgUrl={project.imgUrl}
-                        gitUrl={project.gitUrl}
-                        previewUrl={project.previewUrl}
-                    />
-                ))}
-            </div>
-        </>
-    );
-}
+const ProjectsSection = () => {
+    const [tag, setTag] = useState("All");
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
 
-export default ProjectSection;
+    const handleTagChange = (newTag: string) => {
+        setTag(newTag);
+    };
+
+    const filteredProjects = projectsData.filter((project) =>
+        project.tag.includes(tag)
+    );
+
+    const cardVariants = {
+        initial: { y: 50, opacity: 0 },
+        animate: { y: 0, opacity: 1 },
+    };
+
+    return (
+        <section id="projects">
+            <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+                My Projects
+            </h2>
+            <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="All"
+                    isSelected={tag === "All"}
+                />
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="Web"
+                    isSelected={tag === "Web"}
+                />
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="Mobile"
+                    isSelected={tag === "Mobile"}
+                />
+            </div>
+            <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+                {filteredProjects.map((project: Project, index: number) => (
+                    <motion.li
+                        key={index}
+                        variants={cardVariants}
+                        initial="initial"
+                        animate={isInView ? "animate" : "initial"}
+                        transition={{ duration: 0.3, delay: index * 0.4 }}
+                    >
+                        <ProjectCard
+                            key={project.id}
+                            title={project.title}
+                            description={project.description}
+                            imgUrl={project.imgUrl}
+                            gitUrl={project.gitUrl}
+                            previewUrl={project.previewUrl}
+                        />
+                    </motion.li>
+                ))}
+            </ul>
+        </section>
+    );
+};
+
+export default ProjectsSection;
--- app/components/TabButton.tsx
+++ app/components/TabButton.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react';
+import { motion } from "framer-motion";
 
+const variants = {
+    default: { width: 0 },
+    active: { width: "calc(100% - 0.75rem)" },
+};
 
 interface TabButtonProps {
     active: boolean;
@@ -8,15 +13,19 @@ interface TabButtonProps {
 }
 
 const TabButton: FC<TabButtonProps> = ({ active, selectTab, children }) => {
-    const buttonClasses = active ? 'text-white border-b border-purple-500' : 'text-[#ADB7BE]';
+    const buttonClasses = active ? "text-white" : "text-[#ADB7BE]";
+
     return (
-        <button
-            onClick={selectTab}
-        >
-            <p className={`mr-3 font-semibold ${buttonClasses} `}>
+        <button onClick={selectTab}>
+            <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
                 {children}
             </p>
-        </button >
+            <motion.div
+                animate={active ? "active" : "default"}
+                variants={variants}
+                className="h-1 bg-primary-500 mt-2 mr-3"
+            ></motion.div>
+        </button>
     );
 }
 
