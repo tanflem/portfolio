f5bfd83369f57b93eb432f185274c47cf04a2da0
Tanner <tanner.fleming@gmail.com>
2024-03-08 22:51:54 -0600

    Add node environment to .eslintrc.json and remove unused import in send route.js

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server";
 import { Resend } from "resend";
+import { NextResponse } from "next/server";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
+export async function POST(req) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
   try {
